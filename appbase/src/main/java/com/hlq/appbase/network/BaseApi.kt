c8b9e4a7e3761d66326d139fed5d94b8a7cd65f7
huanglinqing <huanglinqing@zepp.com>
2022-09-14 09:48:22 +0800

    移除点击阴影

--- appbase/src/main/java/com/hlq/appbase/network/BaseApi.kt
+++ appbase/src/main/java/com/hlq/appbase/network/BaseApi.kt
@@ -29,6 +29,6 @@ object BaseApi {
     //主地址
     const val MAIN_API = "http://apis.juhe.cn/springTravel/"
 
-    //请求key
-    const val KEY = "f4b5557f9e2c29defe982dd94b8f22cf"
+    //请求key 关注微信公众号 "Android技术圈" 回复KEY 获取
+    const val KEY = ""
 }
--- module_mine/src/main/java/com/hlq/module_mine/ui/screen/mine/WeChatDialog.kt
+++ module_mine/src/main/java/com/hlq/module_mine/ui/screen/mine/WeChatDialog.kt
@@ -1,16 +1,20 @@
 package com.hlq.module_mine.ui.screen.mine
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -36,18 +40,25 @@ fun WeChatDialog(showDialog: Boolean, onChange: (Boolean) -> Unit) {
                         .height(170.dp)
                         .padding(20.dp)
                 ) {
-                    Text(
-                        text = "欢迎关注公众号“Android技术圈”",
+                    Box(
+                        contentAlignment = Alignment.Center,
                         modifier = Modifier
                             .weight(1f)
-                            .align(Alignment.CenterHorizontally),
-                    )
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "欢迎关注公众号“Android技术圈”",
+                            textAlign = TextAlign.Center
+                        )
+                    }
 
                     Text(
-                        text = "我知道了", modifier = Modifier
-                            .clickable {
+                        text = "知道了", modifier = Modifier
+                            .clickable(indication = null, interactionSource = remember {
+                                MutableInteractionSource()
+                            }, onClick = {
                                 onChange.invoke(false)
-                            }
+                            })
                             .fillMaxWidth(), textAlign = TextAlign.Center
                     )
                 }
